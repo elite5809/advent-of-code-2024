052d5f454d55cb9917583f6233dfe3761eb00aea
elite5809 <20957699+elite5809@users.noreply.github.com>
2024-12-09 11:04:43 -0500

    Clean up comments

--- day03.ps1
+++ day03.ps1
@@ -6,8 +6,7 @@ function Get-AllMulOpMatches ($in) {
     $regexMulOp = 'mul\(\d{1,3},\d{1,3}\)'
     $matchesMulOp = [regex]::Matches($in, $regexMulOp)
     $total = 0
-    
-    # Part 1
+
     $pairs = $matchesMulOp -replace '(mul|[\(\))])'
     foreach ($pair in $pairs) {
         $pair = $pair -split ',' | ForEach-Object { [int]$_ }
@@ -22,4 +21,3 @@ Get-AllMulOpMatches $data
 $regexEnabled = "^.*?(?<=don't\(\))|(?=do\(\)).*?(?<=don't\(\))|(?=do\(\)).*?$"
 $matchesEnabled = [regex]::Matches($data, $regexEnabled)
 Get-AllMulOpMatches $matchesEnabled
-# $matchesEnabled
