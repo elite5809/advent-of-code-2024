e41110b752bd512303b932224dd9b3990c3d896a
elite5809 <20957699+elite5809@users.noreply.github.com>
2024-12-09 10:58:28 -0500

    Finished Day 03
    Regex is a miracle worker

--- day03.ps1
+++ day03.ps1
@@ -1,4 +1,25 @@
-# Day 01 Part 1
-$inFile = '.\data\day01.txt'
+# Day 03
+$inFile = '.\data\day03.txt'
 $data = Get-Content $inFile
 
+function Get-AllMulOpMatches ($in) {
+    $regexMulOp = 'mul\(\d{1,3},\d{1,3}\)'
+    $matchesMulOp = [regex]::Matches($in, $regexMulOp)
+    $total = 0
+    
+    # Part 1
+    $pairs = $matchesMulOp -replace '(mul|[\(\))])'
+    foreach ($pair in $pairs) {
+        $pair = $pair -split ',' | ForEach-Object { [int]$_ }
+        $total += $pair[0] * $pair[1]
+    }
+    $total
+}
+
+# Part 1
+Get-AllMulOpMatches $data
+# Part 2
+$regexEnabled = "^.*?(?<=don't\(\))|(?=do\(\)).*?(?<=don't\(\))|(?=do\(\)).*?$"
+$matchesEnabled = [regex]::Matches($data, $regexEnabled)
+Get-AllMulOpMatches $matchesEnabled
+# $matchesEnabled
